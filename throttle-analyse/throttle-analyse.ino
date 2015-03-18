a6c0336ed0be655c92144af50245a57bc6ddec85
Gerry Kavanagh <gerryk@gmail.com>
2015-03-18 07:22:51 +0000

    changed to handle LOW button activation sense (reversed from previous sketch)

--- throttle-analyse/throttle-analyse.ino
+++ throttle-analyse/throttle-analyse.ino
@@ -8,7 +8,7 @@
    
 */
 // 2-dimensional array of row pin numbers:
-const int row[8] = { 1,2,3,4,5,6,7,8 };
+const int row[8] = { 0,1,2,3,4,5,6,7 };
 
 // 2-dimensional array of column pin numbers:
 const int col[3] = { 18,19,20 };
@@ -20,19 +20,45 @@ void setup() {
   Serial.println("SFS Throttle Button analyser");
   delay(1000);
   for (int x = 0; x < 3; x++) {
-    pinMode(col[x], INPUT);
+    pinMode(col[x], OUTPUT);
   } 
-  Serial.println("COL init");
   delay(1000);
   for (int x = 0; x < 8; x++) {
-    pinMode(row[x], OUTPUT);  
+    pinMode(row[x], INPUT_PULLUP);  
   }
-  Serial.println("ROW init");
 }
 
 
 // loop() runs for as long as power is applied
 void loop() { 
+  digitalWrite(col[0], 0);
+  digitalWrite(col[1], 1);       
+  digitalWrite(col[2], 1);      
+   for (int y = 0; y < 8; y++) {
+     if (digitalRead(row[y])==0)  {
+        Serial.println(y+1);
+      }
+    }
+  
+  digitalWrite(col[0], 1);      
+  digitalWrite(col[1], 0);       
+  digitalWrite(col[2], 1);      
+   for (int y = 0; y < 8; y++) {
+     if (digitalRead(row[y])==0)  {
+        Serial.println(y+9);
+      }
+    }
+
+  digitalWrite(col[0], 1);      
+  digitalWrite(col[1], 1);       
+  digitalWrite(col[2], 0);      
+   for (int y = 0; y < 8; y++) {
+     if (digitalRead(row[y])==0)  {
+        Serial.println(y+17);
+      }
+    }
+  
+ /*
   for (int x = 0; x < 3; x++) {
     for (int y = 0; y < 8; y++) {
      Serial.print("Button ");
@@ -47,4 +73,6 @@ void loop() {
      digitalWrite(row[y], LOW);      
     }
   }
+  
+  */
 }
